575a897a54971862f30cffc836861de46118fe5b
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-01-24 21:44:05 +0700

    Build intent and display data

---
+++ app/src/main/java/com/rujirakongsomran/kt_constraintlayoutform/DisplayMessageActivity.kt
@@ -0,0 +1,27 @@
+package com.rujirakongsomran.kt_constraintlayoutform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rujirakongsomran.kt_constraintlayoutform.databinding.ActivityDisplayMessageBinding
+
+class DisplayMessageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDisplayMessageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_display_message)
+        binding = ActivityDisplayMessageBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val firstname = intent.getStringExtra(EXTRA_FIRSTNAME)
+        val lastname = intent.getStringExtra(EXTRA_LASTNAME)
+        val birthdate = intent.getStringExtra(EXTRA_BIRTHDATE)
+        val country = intent.getStringExtra(EXTRA_COUNTRY)
+
+        binding.tvFirstname.text = firstname
+        binding.tvLastname.text = lastname
+        binding.tvBirthdate.text = birthdate
+        binding.tvCountry.text = country
+
+    }
+}
--- app/src/main/java/com/rujirakongsomran/kt_constraintlayoutform/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_constraintlayoutform/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.rujirakongsomran.kt_constraintlayoutform
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.rujirakongsomran.kt_constraintlayoutform.databinding.ActivityMainBinding
 
+const val EXTRA_FIRSTNAME = "com.rujirakongsomran.kt_constraintlayoutform.FIRSTNAME"
+const val EXTRA_LASTNAME = "com.rujirakongsomran.kt_constraintlayoutform.LASTNAME"
+const val EXTRA_BIRTHDATE = "com.rujirakongsomran.kt_constraintlayoutform.BIRTHDATE"
+const val EXTRA_COUNTRY = "com.rujirakongsomran.kt_constraintlayoutform.COUNTRY"
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +17,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.btnApply.setOnClickListener {
+            sendMessage()
+        }
+    }
+
+    private fun sendMessage(){
+        val firstname = binding.etFirstname.text.toString()
+        val lastname = binding.etLastname.text.toString()
+        val birthdate = binding.etBirthdate.text.toString()
+        val country = binding.etCountry.text.toString()
+
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_FIRSTNAME, firstname)
+            putExtra(EXTRA_LASTNAME, lastname)
+            putExtra(EXTRA_BIRTHDATE, birthdate)
+            putExtra(EXTRA_COUNTRY, country)
+        }
+        startActivity(intent)
     }
 
 }
